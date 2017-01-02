3be500c9936a7f5c3ffffc50f860ee5d5bb01039
lucaskup <lucas.kup@gmail.com>
2017-01-02 21:17:51 -0200

    hackerrank day 1 statistics, standard deviation

---
+++ standardDev.py
@@ -0,0 +1,20 @@
+mport sys
+def mean(x):
+    m = 0
+    
+    for num in x:
+        m += num
+    m = m / len(x)
+    return m
+def standardDev(x):
+    m = mean(x)
+    stdDev = 0
+    for num in x:
+        stdDev += (num - m)**2
+    stdDev = (stdDev/n)**(1/2)
+    return stdDev
+
+n = int(input())
+x = [int(c_temp) for c_temp in input().strip().split(' ')]
+
+print("%.1f" % standardDev(x))
