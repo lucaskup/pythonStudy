3c18b931804d86576d3c9335e07a86733dedef9b
lucaskup <lucas.kup@gmail.com>
2017-01-01 22:34:58 -0200

    second exercise, this time reads two lists and takes the weighted mean

---
+++ weightedMean.py
@@ -0,0 +1,12 @@
+nt(input())
+x = [int(c_temp) for c_temp in input().strip().split(' ')]
+w = [int(c_temp) for c_temp in input().strip().split(' ')]
+
+
+sumWeights = 0
+weightedMean = 0
+
+for i in range(n):
+    weightedMean += x[i]*w[i]
+    sumWeights += w[i]
+print(round(weightedMean/sumWeights,1))
